7ccb9334cd3db0a98fdb36849c8ed5eec3d5dfa7
Mateo-Omeri <mateome96@gmail.com>
2025-10-09 19:40:15 +0200

    feat: add health check endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from .api import health
 
 app = FastAPI(title = 'Todo API')
 
+app.include_router(health.router, prefix = "/api/v1")
+
 @app.get("/")
-def root():
+async def root():
     return {"message": "Welcome to the Todo API!"}
