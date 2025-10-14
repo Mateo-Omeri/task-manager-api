653de7b1c3947b8f5899f98ad09cf9838b049adf
Mateo-Omeri <mateome96@gmail.com>
2025-10-14 19:28:44 +0200

    feat: add CRUD endpoint for adding a task

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -2,11 +2,16 @@ from fastapi import APIRouter
 from app.models.task import Task
 from typing import List
 
-router = APIRouter(tags = ["tasks"])
+router = APIRouter(prefix = "/tasks", tags = ["tasks"])
 
 #temporary in-memory storge
 tasks: list[Task] = []
 
-@router.get("/tasks", response_model=List[Task])
+@router.get("/", response_model=List[Task])
 async def get_all_tasks():
     return tasks
+
+@router.post("/", response_model=Task)
+async def create_task(task: Task):
+    tasks.append(task)
+    return task
