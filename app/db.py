ba1c78a804d0ec63e843ea9ff70fe2cc82f19e09
Mateo-Omeri <mateome96@gmail.com>
2025-10-21 20:19:56 +0200

    feat: integrate SQLAlchemy and create Task table

--- app/db.py
+++ app/db.py
@@ -1,2 +1,19 @@
-# app/db.py
-# Define engine, SessionLocal, Base, and get_db() here
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todo.db'
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
