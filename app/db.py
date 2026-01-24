c9f2ef6245a840d6b6386eea48b36abcc3b30b93
Mateo-Omeri <mateome96@gmail.com>
2026-01-24 15:52:05 +0100

    chore: add docker-compose stack with app and postgres

--- app/db.py
+++ app/db.py
@@ -1,11 +1,15 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./todo.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
